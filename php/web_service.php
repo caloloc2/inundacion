5c15f46d84a1424b2d4be38aa5b55112bb1a2167
caloloc2 <calolomino@gmail.com>
2018-08-22 16:22:00 -0500

    actualiza

---
+++ php/web_service.php
@@ -0,0 +1,18 @@
+<?php
+
+try{
+	require 'meta.php';
+
+	//$fecha_inicio = $_POST['fecha_inicio'];
+	//$fecha_final = $_POST['fecha_final'];	
+
+	//$datos = Meta::Consulta("SELECT * FROM datos WHERE (fecha BETWEEN '".$fecha_inicio."' AND '".$fecha_final."') ORDER BY id_dato ASC");
+	$datos = Meta::Consulta("SELECT * FROM datos ORDER BY id_dato ASC");
+
+	$respuesta = $datos;
+
+}catch(Exception $e){
+	$respuuesta['error'] = $e-getMessage();
+}
+
+echo json_encode($respuesta);
