a8b1bb8c285322b5dbe39d224dabd1865016c236
caloloc2 <calolomino@gmail.com>
2018-08-22 16:35:48 -0500

    actualiza

--- php/web_service.php
+++ php/web_service.php
@@ -3,11 +3,10 @@
 try{
 	require 'meta.php';
 
-	//$fecha_inicio = $_POST['fecha_inicio'];
-	//$fecha_final = $_POST['fecha_final'];	
+	$fecha_inicio = $_POST['fecha_inicio'];
+	$fecha_final = $_POST['fecha_final'];	
 
-	//$datos = Meta::Consulta("SELECT * FROM datos WHERE (fecha BETWEEN '".$fecha_inicio."' AND '".$fecha_final."') ORDER BY id_dato ASC");
-	$datos = Meta::Consulta("SELECT * FROM datos ORDER BY id_dato ASC");
+	$datos = Meta::Consulta("SELECT * FROM datos WHERE (fecha BETWEEN '".$fecha_inicio."' AND '".$fecha_final."') ORDER BY id_dato DESC");	
 
 	$respuesta = $datos;
 
