bc0698617f90428f23dc53f09923b0b3856e3b83
caloloc2 <calolomino@gmail.com>
2020-09-14 20:47:18 -0500

    act

--- php/login.php
+++ php/login.php
@@ -4,15 +4,14 @@ $respuesta['estado'] = false;
 
 try{
 	require 'meta.php';
-	
+
 	$correo = $_REQUEST['correo'];
 	$password = $_REQUEST['password'];
 
 	$inicio = Meta::Consulta_Unico("SELECT * FROM usuarios WHERE ((correo='".$correo."') AND (clave='".$password."'))");
 
 	if ($inicio['id_usuario']!=''){
-		$respuesta['usuario'] = $inicio;
-		$respuesta['estado'] = true;
+		$respuesta['usuario'] = $inicio;		
 	}
 	
 }catch(Exception $e){
